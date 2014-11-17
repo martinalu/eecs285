fdc99cf45820e9708dbf191d46b36490e0d6b0ae
Takudzwa-Rusike <takudzwa495@gmail.com>
2014-11-17 11:15:38 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+package eecs285.proj4.GUI;
+
+import java.awt.*;
+import javax.swing.*;
+import eecs285.proj4.GUI.main_window;
+
+public class Main
+{
+  public static main_window win;
+  
+  public static void main( String[] args )  {
+    win = new main_window();
+    win.setMinimumSize(new Dimension(1250, 800));
+    win.pack();
+    win.setVisible(true);
+    win.setResizable(false);
+    win.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    win.pack();
+  }
+
+}
