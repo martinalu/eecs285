8dd318922a8b433df7c599d4cd8769f96ec8c9fd
the-alex <thealex@umich.edu>
2014-11-17 19:53:49 -0500

    asd
    asd

--- proj4/src/eecs285/proj4/DreamTeam/MainFile.java
+++ proj4/src/eecs285/proj4/DreamTeam/MainFile.java
@@ -7,7 +7,7 @@ import eecs285.proj4.DreamTeam.GUI.main_window;
 public class MainFile
 {
   public static main_window win;
-  
+
   public static void main( String[] args )  {
     win = new main_window();
     win.setMinimumSize(new Dimension(690, 530));
@@ -16,8 +16,6 @@ public class MainFile
     win.setResizable(false);
     win.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     win.pack();
-    //rawr
-    //teststsstststs
   }
 
 }
