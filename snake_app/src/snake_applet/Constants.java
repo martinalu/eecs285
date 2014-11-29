d4770a71924ea39da0194debdfafba6165c7ec0c
Takudzwa-Rusike <takudzwa495@gmail.com>
2014-11-29 14:30:15 -0500

    Create Constants.java
    Constants

---
+++ snake_app/src/snake_applet/Constants.java
@@ -0,0 +1,11 @@
+package snake_applet;
+
+public class Constants
+{
+  public final static int BOARD_WIDTH = 300;
+  public final static int BOARD_HEIGHT = 300;
+  public final static int DOT_SIZE = 8;
+  public final static int ALL_LOCS_ON_BOARD = 900;
+  public final static int RAND_POS = 29;
+  public final static int DELAY = 140;
+}
